063226b33ddd4a7ea43ea3869e472a0da70b5b38
Dima9412 <165942648+Dima9412@users.noreply.github.com>
2024-09-22 16:52:53 +0300

    Initial commit

---
+++ src/StudentController.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class StudentController implements UserController<Student> {
+    private final StudentGroupService studentGroupService = new StudentGroupService();
+    private final StreamService streamService = new StreamService();
+    private final StudentView studentView = new StudentView();
+
+    public void removeStudentByFIO(String firstName, String lastName, String middleName) {
+        studentGroupService.removeStudentByFIO(firstName, lastName, middleName);
+    }
+
+    public List<Student> getSortedStudentList() {
+        List<Student> studentList = studentGroupService.getSortedStudentList();
+        studentView.sendOnConsole(studentList);
+        return studentList;
+    }
+
+    @Override
+    public void create(String firstName, String lastName, String middleName) {
+        studentGroupService.createStudent(firstName, lastName, middleName);
+    }
+}
+
+//Здесь применен принцип SRP. StudentController отвечает только за
+//взаимодействие с сервисами и представлением, но не за саму логику работы
+//с данными, которая делегируется в StudentGroupService и StreamServi
